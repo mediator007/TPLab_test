419ce1770a27e982e368b377e69b51a70c01764f
mediator007 <mediator007@mail.ru>
2022-06-28 11:09:42 +0300

    Selenium unstable work in progress

--- app/services/screenshot.py
+++ app/services/screenshot.py
@@ -26,17 +26,27 @@ def screenshot(url: str, user_id: str) -> str:
     try:
 
         logger.info("driver up")
-        time.sleep(2)
         current_date = datetime.now()
+        # fix date format for file name
         format_date = current_date.strftime("%d_%m_%Y")
+        # format url for request
         format_url_for_file= url.replace('.', '_').replace('http://', '').replace('www.', '')
         url_for_request = url_transform(url)
+        # generate file name and path for saving
         file_name = (f'./app/services/media/{format_date}_{user_id}_{format_url_for_file}.png')
+        # open browser
+
+        # TRY: driver.set_page_load_timeout(30)
+
+        
         driver.get(url_for_request)
-        logger.info(f"get url: {url_for_request}")
         time.sleep(2)
-        driver.get_screenshot_as_file(filename=file_name)
+        logger.info(f"get url: {url_for_request}")
+        # make screenshot and save to path from file_name
+        driver.get_screenshot_as_file(filename=file_name) # try: get_screenshot(filename=file_name)
         logger.info(f"get screenshot name: {file_name}")
+    except Exception as e:
+        logger.error(f"Error in screenshot: {url_for_request}")
     finally:
         driver.quit()
         return file_name
